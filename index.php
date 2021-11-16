ebad8bd86338350f6cd4af6d79b250f28d14404e
apg <gioulisapo@gmail.com>
2021-11-16 22:24:11 +0000

    sanitise

--- index.php
+++ index.php
@@ -38,11 +38,15 @@ foreach ($data as $key => $val) {
             }
             ?>
         </select>
-        <input type="text" placeholder="Amount" name="amount" required>
+        <input type="text" placeholder="Ammount" value="<?php echo (isset($_GET['amount']) ? $_GET['amount'] : ''); ?>" name="amount" required>
         <select name="market_coin">
             <?php
             foreach (array_unique($base_pairs) as &$value) {
-                echo ('<option>' . $value . '</option>');
+                if (isset($_GET['base_coin']) and trim($value) == trim($_GET['base_coin'])) {
+                    echo ('<option selected>' . $value . '</option>');
+                } else {
+                    echo ('<option>' . $value . '</option>');
+                }
             }
             ?>
         </select>
@@ -50,23 +54,23 @@ foreach ($data as $key => $val) {
         <div>
             <?php
             if (isset($_GET['equals'])) {
-                $from_coin = $_GET['base_coin'];
-                $to_coin = $_GET['market_coin'];
-                $amount = $_GET['amount'];
+                $from_coin = filter_input(INPUT_GET,'base_coin',FILTER_SANITIZE_STRIPPED);
+                $to_coin = filter_input(INPUT_GET,'market_coin',FILTER_SANITIZE_STRIPPED);
+                $amount = filter_input(INPUT_GET,'amount',FILTER_SANITIZE_NUMBER_FLOAT);
                 $result = 0.0;
                 if (array_key_exists($from_coin . '_' . $to_coin, $data)) {
                     $result = (float)$data[$from_coin . '_' . $to_coin]['last'] * (float)$amount;
                     printf("%f", $result); // floating point representation
-                } elseif(array_key_exists($to_coin . '_' . $from_coin, $data)) {
-                    $result = round(1/(float)$data[$to_coin . '_' . $from_coin]['last'] * (float)$amount, 8);
+                } elseif (array_key_exists($to_coin . '_' . $from_coin, $data)) {
+                    $result = round(1 / (float)$data[$to_coin . '_' . $from_coin]['last'] * (float)$amount, 8);
                     printf("%f", $result); // floating point representation
                 } else {
-                    echo('Not Valid Pair');
+                    echo ('Not Valid Pair'. $from_coin.'_'.$to_coin);
                 }
             } else {
                 echo '0';
             }
-
+            
             $data = json_decode(file_get_contents('https://xeniosmarkets.com/api/public\?command\=returnTicker'), true);
             print_r($data);
             echo $data;
@@ -77,3 +81,7 @@ foreach ($data as $key => $val) {
 </body>
 
 </html>
+
+<?php
+
+?>
